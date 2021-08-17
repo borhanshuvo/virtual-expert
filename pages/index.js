9755ea34bdf3e7c65a0722969a61e0f4c90d3902
Rakib544 <md.rakib10122003@gmail.com>
2021-08-17 17:01:06 +0600

    new comit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Head from 'next/head';
-
-import Navbar from '../src/components/navbar/navbar';
-import Banner from '../src/components/home/banner/banner';
-import Amazon from '../src/components/home/amazon/amazon';
-import Testimonial from '../src/components/home/testimonial/testimonial';
-import ScheduleMeeting from '../src/components/home/scheduleMeeting/scheduleMeeting';
-import Footer from '../src/components/footer/footer';
+import Head from "next/head";
+import Amazon from "../src/components/home/amazon/amazon";
+import Banner from "../src/components/home/banner/banner";
+import ScheduleMeeting from "../src/components/home/scheduleMeeting/scheduleMeeting";
+import Testimonial from "../src/components/home/testimonial/testimonial";
 
 export default function Home() {
   return (
@@ -16,12 +13,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <Navbar />
         <Banner />
         <Amazon />
         <Testimonial />
         <ScheduleMeeting />
-        <Footer />
       </main>
     </>
   );
--- pages/services.js
+++ pages/services.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import Navbar from '../src/components/navbar/navbar';
-import Header from '../src/components/services/header/header';
-import ServicesCard from '../src/components/services/servicesCard/servicesCard';
-import WhatWeDo from '../src/components/services/whatWeDo/whatWeDo';
+import React from "react";
+import Header from "../src/components/services/header/header";
+import ServicesCard from "../src/components/services/servicesCard/servicesCard";
+import WhatWeDo from "../src/components/services/whatWeDo/whatWeDo";
 
 const Services = () => {
-    return (
-        <>
-            <Navbar />
-            <Header />
-            <WhatWeDo />
-            <ServicesCard />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <WhatWeDo />
+      <ServicesCard />
+    </>
+  );
 };
 
 export default Services;
