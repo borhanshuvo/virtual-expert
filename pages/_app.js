f6ab30d8267cae86a9326463ecc3771bbb32287a
borhanshuvo <borhan015@gmail.com>
2021-08-18 02:12:07 +0600

    kamon vai

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Footer from "../src/components/footer/footer";
 import Navbar from "../src/components/navbar/navbar";
 import "../styles/globals.css";
-import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -15,16 +14,11 @@ function MyApp({ Component, pageProps }) {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous"
         ></link>
-        <script
+        <script async
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
           crossOrigin="anonymous"
         ></script>
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
-          crossOrigin="anonymous"
-        ></Script>
       </Head>
       <Navbar />
       <Component {...pageProps} />
