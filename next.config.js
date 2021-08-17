d9d17940a7eb6fb728528e85643edcf06386e3a5
AhmedKamon <ahmedkamon3@gmail.com>
2021-08-18 01:09:22 +0600

    about section

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'www.linkpicture.com',
+      'lh3.googleusercontent.com',
+      'media.istockphoto.com',
+      'i.pinimg.com',
+    ],
+  },
+};
