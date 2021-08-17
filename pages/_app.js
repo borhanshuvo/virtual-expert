3f9514bd942186a7d1dbed91bcbca70bc9b8aafb
borhanshuvo <borhan015@gmail.com>
2021-08-18 02:06:02 +0600

    no changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
-import Script from "next/script";
 import Footer from "../src/components/footer/footer";
 import Navbar from "../src/components/navbar/navbar";
 import "../styles/globals.css";
@@ -15,11 +14,7 @@ function MyApp({ Component, pageProps }) {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous"
         ></link>
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossOrigin="anonymous"
-        ></Script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
       </Head>
       <Navbar />
       <Component {...pageProps} />
