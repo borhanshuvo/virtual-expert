1fc8231cc94a6dec0ac329175c069e4fa6be6665
Rakib544 <md.rakib10122003@gmail.com>
2021-08-17 16:33:25 +0600

    testimonila section functionality done

--- src/components/testimonial/testimonial.js
+++ src/components/testimonial/testimonial.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
+import { useState } from "react";
 import quoteIcon from "../../../images/icon.svg";
 import profileImg from "../../../images/Img.svg";
+import { testimonials } from "../fakeData/testimonialData";
 
 const Testimonial = () => {
+  const [currentTestimonials, setCurrentTestimonial] = useState(
+    testimonials[0]
+  );
+
+  const handleChange = (id) => {
+    setCurrentTestimonial(testimonials[id]);
+  };
+
   return (
     <section>
       <div className="container py-5">
@@ -22,12 +32,37 @@ const Testimonial = () => {
               className="rounded-circle borderColor "
             />
             <p className="fst-italic my-3 fs-14 lh-lg">
-              “We have been attacked by our competitor We have ordered for
-              Amazon Negative Review Removal service from Virtual Experts they
-              help us within 72 hours.”
+              “{currentTestimonials.text}”
             </p>
-            <h6 className="fw-bold fs-18">Benjamin Arneberg</h6>
-            <p className="fs-14">CEO of PetHonesty</p>
+            <h6 className="fw-bold fs-18">{currentTestimonials.name}</h6>
+            <p className="fs-14">{currentTestimonials.jobTitle}</p>
+          </div>
+          <div className="position-absolute bottom-0 right-38">
+            {testimonials.map((data, index) => (
+              <div
+                key={data.id}
+                className="cursor-pointer"
+                onClick={() => handleChange(index)}
+              >
+                {currentTestimonials.id === data.id ? (
+                  <Image
+                    src={data.image}
+                    alt={data.id}
+                    height="30"
+                    width="30"
+                    className="rounded-circle border border-warning"
+                  />
+                ) : (
+                  <Image
+                    src={data.image}
+                    alt={data.id}
+                    height="24"
+                    width="24"
+                    className="rounded-circle border border-warning"
+                  />
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
