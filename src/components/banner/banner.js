9e6d8607a9e3b304a208cb5970cf9cb3f98fdef6
borhanshuvo <borhan015@gmail.com>
2021-08-17 14:12:02 +0600

    font size fixed

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -11,10 +11,10 @@ const Banner = () => {
         <div className="container py-5">
           <div className="row d-flex align-items-center">
             <div className="col-md-6 order-2 order-md-1">
-              <h6>Amazon FBA/Kindle & Digital Markketing Consultant.</h6>
-              <p>Grow your brands on Amazon!</p>
+              <h6 className="fs-36">Amazon FBA/Kindle & Digital Markketing Consultant.</h6>
+              <p className="fs-15">Grow your brands on Amazon!</p>
               <button className="button px-4 py-1">
-                <p className="d-inline">Get Free Quote!</p>
+                <p className="d-inline fs-14">Get Free Quote!</p>
               </button>
             </div>
             <div className="col-md-6 order-1 order-md-2 pb-5 pb-md-0">
