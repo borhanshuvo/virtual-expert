c075c344d009f190d93e737fc4c15bcd0dfbe8a4
borhanshuvo <borhan015@gmail.com>
2021-08-18 01:23:58 +0600

    again pera life

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
+import Script from "next/script";
 import Footer from "../src/components/footer/footer";
 import Navbar from "../src/components/navbar/navbar";
 import "../styles/globals.css";
@@ -14,11 +15,11 @@ function MyApp({ Component, pageProps }) {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous"
         ></link>
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
           crossOrigin="anonymous"
-        ></script>
+        ></Script>
       </Head>
       <Navbar />
       <Component {...pageProps} />
