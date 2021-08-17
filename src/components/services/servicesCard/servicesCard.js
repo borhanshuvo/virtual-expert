28847e4f309856c536408e2a2215a8b8239ed553
borhanshuvo <borhan015@gmail.com>
2021-08-17 16:39:34 +0600

    changes

---
+++ src/components/services/servicesCard/servicesCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ServicesCard = () => {
+    return (
+        <>
+            <div className="background-color-skyblue py-5">
+                <h6 className="text-center my-5">Our Amazon FBA/Kindle & Digital Marketing Services</h6>
+            </div>
+        </>
+    );
+};
+
+export default ServicesCard;
