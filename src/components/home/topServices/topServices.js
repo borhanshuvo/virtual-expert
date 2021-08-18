a89d7814e02a23c3db1e9d6a4e0aa0f6ac32ee2e
SAGOR <seakingmawmaw2020@gmail.com>
2021-08-18 13:12:37 +0600

    update backend

--- src/components/home/topServices/topServices.js
+++ src/components/home/topServices/topServices.js
@@ -1,33 +1,11 @@
 import Image from "next/image";
-import img1 from "../../../../public/home_top_service_card_image1.svg";
-import img2 from "../../../../public/home_top_service_card_image2.svg";
-import img3 from "../../../../public/home_top_service_card_image3.svg";
 
 import { RiArrowRightSLine } from 'react-icons/ri'
 import styles from "./topServices.module.css";
 
-const services = [
-  {
-    img: img1,
-    title: "AMAZON CUSTOMER REVIEW",
-    description:
-      "Product reviews engage customers, builds customer trust & help with Amazon SEO. Amazon reviews provide helpful data and insights into customer concerns and want. 75% of U.S. consumers said that, when shopping online…",
-  },
-  {
-    img: img2,
-    title: "REMOVE NEGATIVE AMAZON REVIEWS FROM 1ST PAGE",
-    description:
-      "Did you know that a survey we did in 2021 said that 56% of all Amazon Prime Customers read every single review on your listing’s first page? An additional 24% of people only read the top 3 reviews on your listing’s first page…",
-  },
-  {
-    img: img3,
-    title: "AMAZON SEO - RANK 1ST PAGE IN AMAZON SEARCH ENGINE",
-    description:
-      "We have helped rank thousands of products on Amazon. Every single campaign is met with a one on one, hands-on approach, offering professional advice on which keywords, off-page secrets, product pricing and giveaway…",
-  },
-];
 
-const TopServices = () => {
+const TopServices = ({topServicesData}) => {
+  console.log(topServicesData)
   return (
     <div className="container py-5">
       <div className="py-5">
@@ -35,13 +13,15 @@ const TopServices = () => {
         Our TOP 3 Services for Amazon FBA/Kindle Business
       </h6>
       <div className="row">
-        {services.map((service, index) => (
-          <div key={index} className={`${styles.serviceCard} col-4 p-4  mt-md-5`}>
+        {topServicesData.map((service, index) => (
+          <div key={service._id} className={`${styles.serviceCard} col-4 p-4  mt-md-5`}>
             <div className="d-flex justify-content-center">
               <Image
-                className={`${styles.bannerContainer} img-fluid service-img`}
+                className={`${styles.bannerContainer} img-fluid`}
                 src={service.img}
                 alt=""
+                height={120}
+                width={190}
               />
             </div>
             <h6 className="text-center mt-5">{service.title}</h6>
