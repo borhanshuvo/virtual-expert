820073e78ac0e6721338f47444d3609a20942c6d
borhanshuvo <borhan015@gmail.com>
2021-08-19 21:18:04 +0600

    service added success

--- pages/dashboard/service.js
+++ pages/dashboard/service.js
@@ -1,24 +1,81 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { AiFillEdit, AiOutlinePlus } from "react-icons/ai";
+import { useForm } from "react-hook-form";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Sidebar from "../../src/components/dashboard/sidebar/sidebar";
+import image from "next/image";
 
 const Service = ({ whatWeDo }) => {
   const [serviceBanner, setServiceBanner] = useState({});
   const [servicesCardData, setServicesCardData] = useState([]);
   const [number, setNumber] = useState(0);
+  const [file, setFile] = useState(null);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     fetch("http://localhost:8000/whatWeDo")
       .then((res) => res.json())
       .then((data) => setServiceBanner(data));
 
-    fetch("https://virtual-expert.herokuapp.com/servicesCard")
+    fetch("http://localhost:8000/servicesCard")
       .then((res) => res.json())
       .then((data) => setServicesCardData(data));
   }, [number]);
 
+  const handleFileChange = (e) => {
+    const newFile = e.target.files[0];
+    setFile(newFile);
+  };
+
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("title", data.title);
+    formData.append("subTitle", data.subTitle);
+    formData.append("regularReview", data.regularReview);
+    formData.append("videoReview", data.videoReview);
+    formData.append("top50Reviewers", data.top50Reviewers);
+    formData.append("delivery", data.delivery);
+    formData.append("warranty", data.warranty);
+    formData.append("price", data.price);
+    formData.append("maintenance", data.maintenance);
+
+    fetch(`http://localhost:8000/servicesCard/post`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          toast.success("Service Add Successfully");
+          console.log(result);
+          setNumber(number + 1);
+        }
+      });
+  };
+
   return (
     <section className="overflow-hidden">
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
       <div className="row">
         <div className="col-2">
           <Sidebar />
@@ -40,103 +97,276 @@ const Service = ({ whatWeDo }) => {
           <div className="p-3 boxShadow me-4 mb-5">
             <div className="d-flex justify-content-between">
               <h6 className="fs-24">Service Card</h6>
-              <AiOutlinePlus size={24} className="text-dark" />
+              <AiOutlinePlus
+                size={24}
+                className="text-dark"
+                data-bs-toggle="modal"
+                data-bs-target="#addServiceModal"
+              />
             </div>
             <div className="row">
-              {servicesCardData.map((servicesCard, index) => (
-                <div className="col-md-4 pb-3" key={servicesCard._id}>
-                  <div className="d-flex justify-content-between">
-                    {/* <Image
-                      src={servicesCard.img}
-                      alt={servicesCard.img}
-                      width={200}
-                      height={200}
-                    /> */}
+              {servicesCardData.map((servicesCard, index) => {
+                let imgType;
+                const svg = "data:image/svg+xml";
+                const jpg = "data:image/jpg";
+                if (servicesCard.img.contentType === "image/svg+xml") {
+                  imgType = "data:image/svg+xml";
+                } else if (servicesCard.img.contentType === "image/png") {
+                  imgType = "data:image/png";
+                } else {
+                  imgType = "data:image/jpg";
+                }
+                return (
+                  <div className="col-md-4 pb-3" key={servicesCard._id}>
                     <AiFillEdit
                       size={24}
-                      className="text-warning"
+                      className="text-primary d-block ms-auto"
                       data-bs-toggle="modal"
                       data-bs-target={`#card${index + 1}`}
                     />
-                  </div>
-                  <div
-                    className="bg-white p-3 borderRadius"
-                    style={{ height: "300px" }}
-                  >
-                    <h6 className="fs-18">{servicesCard.title}</h6>
-                    <h6 className="fs-14 mt-2">{servicesCard.subTitle}</h6>
-                    {servicesCard.regularReview && (
-                      <p className="fs-14">
-                        Regular Review : ${servicesCard.regularReview} each
-                      </p>
-                    )}
-                    {servicesCard.videoReview && (
-                      <p className="fs-14">
-                        Video Review : ${servicesCard.videoReview} each
-                      </p>
-                    )}
-                    {servicesCard.top50Reviewers && (
-                      <p className="fs-14">
-                        Top 50 Reviewers : ${servicesCard.top50Reviewers} each
-                      </p>
-                    )}
-                    {servicesCard.price && (
-                      <p className="fs-14">Price : {servicesCard.price}</p>
-                    )}
-                    {servicesCard.delivery && (
-                      <p className="fs-14">
-                        Delivery : {servicesCard.delivery} days
-                      </p>
-                    )}
-                    {servicesCard.warranty && (
-                      <p className="fs-14">
-                        Warranty : {servicesCard.warranty} days free maintenence
-                      </p>
-                    )}
-                    {servicesCard.maintenence && (
-                      <p className="fs-14">
-                        Maintenence : ${servicesCard.maintenence} per month
-                      </p>
-                    )}
-                  </div>
-                  <div
-                    className="modal fade"
-                    id={`card${index + 1}`}
-                    tabIndex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog modal-dialog-centered">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h5 className="modal-title" id="exampleModalLabel">
-                            Modal title
-                          </h5>
-                          <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                          ></button>
-                        </div>
-                        <div className="modal-body">...</div>
-                        <div className="modal-footer">
-                          <button
-                            type="button"
-                            className="btn btn-secondary"
-                            data-bs-dismiss="modal"
-                          >
-                            Close
-                          </button>
-                          <button type="button" className="btn btn-primary">
-                            Save changes
-                          </button>
+
+                    <Image
+                      src={`${imgType} ; base64, ${servicesCard.img.img}`}
+                      alt="waiting.."
+                      width={200}
+                      height={200}
+                    />
+
+                    <div
+                      className="bg-white p-3 borderRadius"
+                      style={{ height: "300px" }}
+                    >
+                      <h6 className="fs-18">{servicesCard.title}</h6>
+                      <h6 className="fs-14 mt-2">{servicesCard.subTitle}</h6>
+                      {servicesCard.regularReview && (
+                        <p className="fs-14">
+                          Regular Review : ${servicesCard.regularReview} each
+                        </p>
+                      )}
+                      {servicesCard.videoReview && (
+                        <p className="fs-14">
+                          Video Review : ${servicesCard.videoReview} each
+                        </p>
+                      )}
+                      {servicesCard.top50Reviewers && (
+                        <p className="fs-14">
+                          Top 50 Reviewers : ${servicesCard.top50Reviewers} each
+                        </p>
+                      )}
+                      {servicesCard.price && (
+                        <p className="fs-14">Price : {servicesCard.price}</p>
+                      )}
+                      {servicesCard.delivery && (
+                        <p className="fs-14">
+                          Delivery : {servicesCard.delivery} days
+                        </p>
+                      )}
+                      {servicesCard.warranty && (
+                        <p className="fs-14">
+                          Warranty : {servicesCard.warranty} days free
+                          maintenence
+                        </p>
+                      )}
+                      {servicesCard.maintenence && (
+                        <p className="fs-14">
+                          Maintenence : ${servicesCard.maintenence} per month
+                        </p>
+                      )}
+                    </div>
+                    <div
+                      className="modal fade"
+                      id={`card${index + 1}`}
+                      tabIndex="-1"
+                      aria-labelledby="exampleModalLabel"
+                      aria-hidden="true"
+                    >
+                      <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                          <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">
+                              Modal title
+                            </h5>
+                            <button
+                              type="button"
+                              className="btn-close"
+                              data-bs-dismiss="modal"
+                              aria-label="Close"
+                            ></button>
+                          </div>
+                          <div className="modal-body">...</div>
+                          <div className="modal-footer">
+                            <button
+                              type="button"
+                              className="btn btn-secondary"
+                              data-bs-dismiss="modal"
+                            >
+                              Close
+                            </button>
+                            <button type="button" className="btn btn-primary">
+                              Save changes
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="modal fade"
+        id="addServiceModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Add Service Card
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                  <label htmlFor="title">Title</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("title")}
+                    name="title"
+                    id="title"
+                    className="form-control"
+                  />
                 </div>
-              ))}
+
+                <div className="form-group">
+                  <label htmlFor="title">Sub Title</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("subTitle")}
+                    name="subTitle"
+                    id="subTitle"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="title">Regular Review</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("regularReview")}
+                    name="regularReview"
+                    id="regularReview"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="title">Video Review</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("videoReview")}
+                    name="videoReview"
+                    id="videoReview"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="title">Top 50 Reviewers</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("top50Reviewers")}
+                    name="top50Reviewers"
+                    id="top50Reviewers"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="title">Delivery</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("delivery")}
+                    name="delivery"
+                    id="delivery"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="title">Warranty</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("warranty")}
+                    name="warranty"
+                    id="warranty"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="title">Price</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("price")}
+                    name="price"
+                    id="price"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="title">Maintenance</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    {...register("maintenance")}
+                    name="maintenance"
+                    id="maintenance"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="img">Image</label>
+                  <input
+                    type="file"
+                    name="img"
+                    id="img"
+                    className="form-control"
+                    onChange={handleFileChange}
+                  />
+                </div>
+
+                <div className="form-group mt-3">
+                  <input
+                    type="submit"
+                    name="submit"
+                    className="btn btn-primary"
+                    value="Save Changes"
+                    data-bs-dismiss="modal"
+                  />
+                </div>
+              </form>
             </div>
           </div>
         </div>
