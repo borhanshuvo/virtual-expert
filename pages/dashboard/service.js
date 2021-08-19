f3f0eb933f755fc4b5e5e794844c97c29d70af6a
borhanshuvo <borhan015@gmail.com>
2021-08-20 01:34:13 +0600

    mela changes

--- pages/dashboard/service.js
+++ pages/dashboard/service.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { AiFillEdit, AiOutlinePlus } from "react-icons/ai";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { GoPlus } from "react-icons/go";
 import { useForm } from "react-hook-form";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Sidebar from "../../src/components/dashboard/sidebar/sidebar";
+import AdminServiceCard from "../../src/components/dashboard/service/adminServiceCard/adminServiceCard";
 
-const Service = ({ whatWeDo }) => {
+const Service = () => {
   const [serviceBanner, setServiceBanner] = useState({});
   const [servicesCardData, setServicesCardData] = useState([]);
   const [number, setNumber] = useState(0);
@@ -82,7 +84,7 @@ const Service = ({ whatWeDo }) => {
           <div className="p-3 boxShadow me-4 mb-5">
             <div className="d-flex justify-content-between">
               <h6 className="fs-24">Service Banner</h6>
-              <AiFillEdit size={24} className="text-warning" />
+              <AiFillEdit size={24} className="text-warning cursor-pointer" />
             </div>
             <h6 className="mt-3 fs-18">Title</h6>
             <p classNames="fs-14">{serviceBanner.title}</p>
@@ -95,9 +97,9 @@ const Service = ({ whatWeDo }) => {
           <div className="p-3 boxShadow me-4 mb-5">
             <div className="d-flex justify-content-between">
               <h6 className="fs-24">Service Card</h6>
-              <AiOutlinePlus
+              <GoPlus
                 size={24}
-                className="text-dark"
+                className="text-dark cursor-pointer"
                 data-bs-toggle="modal"
                 data-bs-target="#addServiceModal"
               />
@@ -113,104 +115,22 @@ const Service = ({ whatWeDo }) => {
                   imgType = "data:image/jpg";
                 }
                 return (
-                  <div className="col-md-4 pb-3" key={servicesCard._id}>
-                    <AiFillEdit
-                      size={24}
-                      className="text-primary d-block ms-auto"
-                      data-bs-toggle="modal"
-                      data-bs-target={`#card${index + 1}`}
-                    />
-
-                    <Image
-                      src={`${imgType} ; base64, ${servicesCard.img.img}`}
-                      alt="waiting.."
-                      width={200}
-                      height={200}
-                    />
-
-                    <div
-                      className="boxShadow p-3 borderRadius"
-                      style={{ height: "300px" }}
-                    >
-                      <h6 className="fs-18">{servicesCard.title}</h6>
-                      <h6 className="fs-14 mt-2">{servicesCard.subTitle}</h6>
-                      {servicesCard.regularReview && (
-                        <p className="fs-14">
-                          Regular Review : ${servicesCard.regularReview} each
-                        </p>
-                      )}
-                      {servicesCard.videoReview && (
-                        <p className="fs-14">
-                          Video Review : ${servicesCard.videoReview} each
-                        </p>
-                      )}
-                      {servicesCard.top50Reviewers && (
-                        <p className="fs-14">
-                          Top 50 Reviewers : ${servicesCard.top50Reviewers} each
-                        </p>
-                      )}
-                      {servicesCard.price && (
-                        <p className="fs-14">Price : {servicesCard.price}</p>
-                      )}
-                      {servicesCard.delivery && (
-                        <p className="fs-14">
-                          Delivery : {servicesCard.delivery} days
-                        </p>
-                      )}
-                      {servicesCard.warranty && (
-                        <p className="fs-14">
-                          Warranty : {servicesCard.warranty}
-                        </p>
-                      )}
-                      {servicesCard.maintenance && (
-                        <p className="fs-14">
-                          Maintenence : ${servicesCard.maintenance} per month
-                        </p>
-                      )}
-                    </div>
-                    <div
-                      className="modal fade"
-                      id={`card${index + 1}`}
-                      tabIndex="-1"
-                      aria-labelledby="exampleModalLabel"
-                      aria-hidden="true"
-                    >
-                      <div className="modal-dialog modal-dialog-centered">
-                        <div className="modal-content">
-                          <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">
-                              Modal title
-                            </h5>
-                            <button
-                              type="button"
-                              className="btn-close"
-                              data-bs-dismiss="modal"
-                              aria-label="Close"
-                            ></button>
-                          </div>
-                          <div className="modal-body">...</div>
-                          <div className="modal-footer">
-                            <button
-                              type="button"
-                              className="btn btn-secondary"
-                              data-bs-dismiss="modal"
-                            >
-                              Close
-                            </button>
-                            <button type="button" className="btn btn-primary">
-                              Save changes
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <AdminServiceCard
+                    servicesCard={servicesCard}
+                    imgType={imgType}
+                    index={index}
+                    servicesCardData={servicesCardData}
+                    setServicesCardData={setServicesCardData}
+                    setNumber={setNumber}
+                  />
                 );
               })}
             </div>
           </div>
         </div>
       </div>
+
+      {/* For Add Card data Start */}
       <div
         className="modal fade"
         id="addServiceModal"
@@ -376,7 +296,7 @@ const Service = ({ whatWeDo }) => {
                     type="submit"
                     name="submit"
                     className="btn btn-primary"
-                    value="Save Changes"
+                    value="Submit"
                   />
                 </div>
               </form>
@@ -384,6 +304,7 @@ const Service = ({ whatWeDo }) => {
           </div>
         </div>
       </div>
+      {/* For Add Card data Finish */}
     </section>
   );
 };
