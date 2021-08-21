6ef576b95927901968827422bc9ef16b06ff7c80
borhanshuvo <borhan015@gmail.com>
2021-08-22 00:18:37 +0600

    changes

--- src/components/services/whatWeDo/whatWeDo.js
+++ src/components/services/whatWeDo/whatWeDo.js
@@ -1,11 +1,10 @@
 import React from "react";
-import styles from "./whatWeDo.module.css";
 
 const WhatWeDo = ({whatWeDo}) => {
   return (
     <>
       <div className="container py-5 my-3">
-        <h6 className={`${styles.title} fs-28 mb-4`}>{whatWeDo?.title}</h6>
+        <h6 className={`textLeftAfterEffect fs-28 mb-4`}>{whatWeDo?.title}</h6>
         <p className="fs-15 text-color my-4 lh-lg">{whatWeDo?.description}</p>
         <h6 className="fs-18 lh-lg">{whatWeDo?.subDescription}</h6>
       </div>
