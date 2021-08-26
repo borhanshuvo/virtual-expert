d395d492315af29b2a75008399261676de5252d4
Rakib544 <md.rakib10122003@gmail.com>
2021-08-26 21:19:21 +0600

    new commit

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,29 +1,29 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import { AiFillInstagram } from 'react-icons/ai';
-import { FaFacebook, FaSkype, FaTelegram } from 'react-icons/fa';
-import { ImTwitter } from 'react-icons/im';
-import { IoLogoWhatsapp } from 'react-icons/io';
-import { MdEmail } from 'react-icons/md';
-import Logo from '../../../images/Logo.svg';
-import BTC from '../../../images/Others/Bitcoin.svg';
-import Payoneer from '../../../images/Others/Payoneer.svg';
-import PayPal from '../../../images/Others/PayPal.svg';
-import skrill from '../../../images/Others/Skrill.svg';
-import styles from './footer.module.css';
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaFacebook, FaSkype, FaTelegram, FaYoutube } from "react-icons/fa";
+import { ImTwitter } from "react-icons/im";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import Logo from "../../../images/Logo.svg";
+import BTC from "../../../images/Others/Bitcoin.svg";
+import Payoneer from "../../../images/Others/Payoneer.svg";
+import PayPal from "../../../images/Others/PayPal.svg";
+import skrill from "../../../images/Others/Skrill.svg";
+import styles from "./footer.module.css";
 
 const Footer = () => {
   const router = useRouter();
   const [footer, setFooter] = useState(null);
   const [footerLink, setFooterLink] = useState({});
   useEffect(() => {
-    fetch('https://sleepy-mesa-08037.herokuapp.com/footer')
+    fetch("https://sleepy-mesa-08037.herokuapp.com/footer")
       .then((res) => res.json())
       .then((data) => setFooter(data[0]));
 
-    fetch('https://sleepy-mesa-08037.herokuapp.com/footerLink')
+    fetch("https://sleepy-mesa-08037.herokuapp.com/footerLink")
       .then((res) => res.json())
       .then((data) => setFooterLink(data[0]));
   }, []);
@@ -31,12 +31,12 @@ const Footer = () => {
   return (
     <footer
       className={
-        router.pathname === '/dashboard' ||
-        router.pathname === '/dashboard/service' ||
-        router.pathname === '/dashboard/about' ||
-        router.pathname === '/dashboard/orders'
-          ? 'd-none'
-          : ''
+        router.pathname === "/dashboard" ||
+        router.pathname === "/dashboard/service" ||
+        router.pathname === "/dashboard/about" ||
+        router.pathname === "/dashboard/orders"
+          ? "d-none"
+          : ""
       }
     >
       <section className={`${styles.main}`}>
@@ -57,21 +57,21 @@ const Footer = () => {
                 </h6>
               </div>
               <p className={`mt-3 text-muted fs-14 lh-30`}>
-                {' '}
+                {" "}
                 <MdEmail className={`${styles.logo}`} />
                 <span className={`${styles.hilight}`}>
                   <span className="fw-500">Email</span> : {footer?.email}
                 </span>
               </p>
               <p className=" text-muted fs-14 lh-30">
-                {' '}
+                {" "}
                 <FaSkype className={`${styles.logo}`} />
                 <span className={`${styles.hilight}`}>
                   <span className="fw-500">Skype</span> : {footer?.skype}
                 </span>
               </p>
               <p className=" text-muted fs-14 lh-30">
-                {' '}
+                {" "}
                 <IoLogoWhatsapp className={`${styles.logo}`} />
                 <span className={`${styles.hilight}`}>
                   <span className="fw-500">WhatsApp</span> : {footer?.whatsApp}
@@ -110,6 +110,13 @@ const Footer = () => {
                   <Link href={footerLink.twitter}>
                     <a target="_blank">
                       <ImTwitter className={`${styles.logo} me-3`} />
+                    </a>
+                  </Link>
+                )}
+                {footerLink?.youTube && (
+                  <Link href={footerLink.youTube}>
+                    <a target="_blank">
+                      <FaYoutube className={`${styles.logo} me-3`} />
                     </a>
                   </Link>
                 )}
